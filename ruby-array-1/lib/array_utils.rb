b4d4fc7d438595336e442da46bd4a2793abee354
joaoalber <jooalberto1@gmail.com>
2019-10-05 01:11:29 -0300

    Desafios ruby feitos

---
+++ ruby-array-1/lib/array_utils.rb
@@ -0,0 +1,31 @@
+class ArrayUtils
+
+  def self.multiplos(qtd, multiplo)
+    arr = []
+    numero = multiplo
+    qtd.times do |i|
+      arr[i] = multiplo
+      multiplo = numero + multiplo
+    end
+    arr
+  end
+
+  def self.tabuada(numero)
+    arr = []
+    tabuada = 1
+    numero.times do |i|
+      subarr = []
+      10.times do |x|
+        subarr[x] = tabuada * (x+1)
+      end
+      arr[i] = subarr
+      tabuada += 1
+    end
+    arr
+  end
+
+  def self.ultimo(lista)
+    lista[lista.size-1]
+  end
+
+end
